0df4f737bc72db98799cdac3d6d9751a693e6931
MichaelWaymire <mike.waymire2@gmail.com>
2018-09-18 14:30:30 -0500

    finsihed CE exercises and started ControlFlow exercisese

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,81 @@
+// in order to use the Scanner method we have to import it.
+import java.awt.*;
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        // double variable
+//        double pi = 3.14159;
+        // prints to the console.
+//        System.out.format("The Value of pi is approvimately %.2f", pi);
+
+
+
+//  1)Prompt a user to enter a integer and store that value in an int
+//    variable using the nextInt method.
+//        Scanner Scanner = new Scanner(System.in);
+//
+//        System.out.println("Please give me a number between 1-100");
+//        //Scanner.nextInt takes in the number given.
+//        int numberGiven = Scanner.nextInt();
+//        //Print out the answer here
+//        System.out.format("/n The number you gave is: " + numberGiven);
+
+
+//  2)Prompt a user to enter 3 words and store each of them in a
+//    separate variable, then display them back, each on a newline.
+
+//        Scanner Scanner = new Scanner(System.in);
+//
+//        System.out.println("Give me three words");
+//        String firstWord = Scanner.next();
+//        System.out.println(firstWord);
+//        String secondWord = Scanner.next();
+//        System.out.println(secondWord);
+//        String thirdWord = Scanner.next();
+//        System.out.println(thirdWord);
+
+//  3) Prompt a user to enter a sentence, then store that sentence in a
+// String variable using the .next method, then display that sentence
+// back to the user.
+
+//        Scanner Scanner = new Scanner(System.in);
+//
+//        System.out.println("Please type: The little red fox jumped over the big gray fence");
+//  4) Rewrite the above example using the .nextLine method.
+//     String sentenceFox = Scanner.next();
+//        String sentenceFox = Scanner.nextLine();
+//        System.out.println(sentenceFox);
+
+
+//        Calculate the perimeter and area of Codeup's classrooms
+
+//  1) Prompt the user to enter values of length and width of a
+// classroom at Codeup.
+//
+//        Scanner Scanner = new Scanner(System.in);
+//        String sentence = Scanner.nextLine();
+//        System.out.println(sentence);
+//
+//        System.out.println("How long is the classroom in feet");
+//        int length = Integer.parseInt(Scanner.nextLine());
+//
+//        System.out.println("How wide is the classroom in feet");
+//        int width = Integer.parseInt(Scanner.nextLine());
+//
+//        System.out.println("The are of classroom is " + length * width);
+//        System.out.println("The perimeter of the classroom is " + (2 * length) +(2 * width));
+
+
+//        System.out.println("How long is the classroom in feet");
+//        int classroomLength = Scanner.nextInt();
+//        System.out.println("How wide is the classroom in feet");
+//        int classroomWidth = Scanner.nextInt();
+//        int classroomSize = classroomLength * 2 + classroomWidth * 2;
+//        System.out.println("This class room size is " + classroomSize);
+
+
+
+    }
+}
--- src/SandBox.java
+++ src/SandBox.java
@@ -1,35 +1,44 @@
+import java.util.Scanner;
+
 public class SandBox {
     public static void main(String[] args){
-        byte veryShort = -128;
-        // byte = -128 to 127
-        short regShort = 32767;
-        // Short integers from -32,768 to 32,767
-        int inters = 2147483647;
-        //Integers from -2,147,483,648 to 2,147,483,647
-        long num = 1231231231123412341L;
-        //Long integers from -9,223,372,036,854,775,808 to
-        // 9,223,372,036,854,775,807
-        float floating = -3.4383234234123434534553453453452345345F;
-        //Single-precision, floating-point numbers from -3.4E38 to
-        // 3.4E38 with up to 7 significant digits
-        double dude = 123123123.12312312333131313;
-        //Double-precision, floating-point numbers from -1.7E308 to 1.7E308 with
-        // up to 16 significant digits
-        char smsh = 1;
-        //A single Unicode character that’s stored in two bytes
-        boolean booleanVar = true;
-        //A true or false value
-        String stringVar = "Hello, Wrangell";
+//        byte veryShort = -128;
+//        // byte = -128 to 127
+//        short regShort = 32767;
+//        // Short integers from -32,768 to 32,767
+//        int inters = 2147483647;
+//        //Integers from -2,147,483,648 to 2,147,483,647
+//        long num = 1231231231123412341L;
+//        //Long integers from -9,223,372,036,854,775,808 to
+//        // 9,223,372,036,854,775,807
+//        float floating = -3.4383234234123434534553453453452345345F;
+//        //Single-precision, floating-point numbers from -3.4E38 to
+//        // 3.4E38 with up to 7 significant digits
+//        double dude = 123123123.12312312333131313;
+//        //Double-precision, floating-point numbers from -1.7E308 to 1.7E308 with
+//        // up to 16 significant digits
+//        char smsh = 1;
+//        //A single Unicode character that’s stored in two bytes
+//        boolean booleanVar = true;
+//        //A true or false value
+//        String stringVar = "Hello, Wrangell";
+//
+//        System.out.println(veryShort);
+//        System.out.println(regShort);
+//        System.out.println(inters);
+//        System.out.println(num);
+//        System.out.println(floating);
+//        System.out.println(dude);
+//        System.out.println(smsh);
+//        System.out.println(booleanVar);
+//        System.out.println(stringVar);
 
-        System.out.println(veryShort);
-        System.out.println(regShort);
-        System.out.println(inters);
-        System.out.println(num);
-        System.out.println(floating);
-        System.out.println(dude);
-        System.out.println(smsh);
-        System.out.println(booleanVar);
-        System.out.println(stringVar);
+        Scanner bananaScanner = new Scanner(System.in);
+        System.out.println("What is your First Name");
+        String firstName = bananaScanner.next();
+        System.out.println("what is your Last name?");
+        String lastName = bananaScanner.next();
+        System.out.format("Hello %s %s", firstName, lastName);
     }
 
 }
