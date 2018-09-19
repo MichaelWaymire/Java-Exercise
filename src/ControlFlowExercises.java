fcfc05772c910002436b713d81f5a0ac9171e8ed
MichaelWaymire <mike.waymire2@gmail.com>
2018-09-19 09:06:11 -0500

    Finsihed step 2 part a and b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,25 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15) {
-            System.out.println(i);
-            i++;
+        int count = 0 ;
+        while ( count <= 98 )
+        {
+            count += 2 ;
+            System.out.println(count);
         }
+
+
 
     }
 }
+
+
+
+
+//    int i = 5;
+//        while(i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//    }
+//}
