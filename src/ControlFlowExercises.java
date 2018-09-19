74633de92313b886834a8aa295a985578e0c8806
MichaelWaymire <mike.waymire2@gmail.com>
2018-09-19 09:49:57 -0500

    Finsihed 2.A

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long count = 2;
-        do {
+        for(int i = 0; i < 101; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
 
-            System.out.println(count);
-            count *= count;
-            
-            }while (count <= 1000000);
+            }
+
+        }
 
     }
 
-
-    }
+}
 
 
 
